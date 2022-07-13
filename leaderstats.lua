203051fbdeea8944871f8447e28b20fbd515588e
Ash <87194661+venexc@users.noreply.github.com>
2022-07-13 19:04:57 +0100

    e

--- leaderstats.lua
+++ leaderstats.lua
@@ -1,3 +1,14 @@
 local Players = game:GetService("Players")
 
-print("E")
+local function leaderboardSetup(player)
+    local leaderstats = Instance.new("Folder")
+    leaderstats.Name = "leaderstats"
+    leaderstats.Parent = player
+
+    local clicks = Instance.new("IntValue")
+    clicks.Name = "Clicks"
+    clicks.Value = 0
+    clicks.Parent = leaderstats
+end
+
+Players.PlayerAdded:Connect(leaderboardSetup)
